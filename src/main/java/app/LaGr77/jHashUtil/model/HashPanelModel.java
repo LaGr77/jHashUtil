3ca57c372eb803b011fd121de6c9453f66c7565d
Ladislav Grulich (LaGr77) <lada.grulich@gmail.com>
2022-01-18 16:47:51 +0100

    2022-01-18 16:47

---
+++ src/main/java/app/LaGr77/jHashUtil/model/HashPanelModel.java
@@ -0,0 +1,21 @@
+package app.LaGr77.jHashUtil.model;
+
+/**
+ * HAsh panel model
+ * @author Ladislav Grulich (LaGr77) <a href="mailto:aaa.bbb@gmail.com">[aaa.bbb@gmail.com]</a>
+ * @version 0.1.0
+ * @since 2022-01-18
+ */
+public class HashPanelModel {
+    
+    /** Constructor */
+    public HashPanelModel () {
+        nothing = true;
+    }
+        
+    /****************************************************************************************************
+     *                                                                                          Variables
+     ****************************************************************************************************/
+    private Boolean nothing;   
+
+}
